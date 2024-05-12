ab8e2cae47cccc1f8c477a27112dd2bd287d8051
wae <69380106+w-a-e@users.noreply.github.com>
2024-05-12 19:50:54 +0300

    use buffer

--- Reader.lua
+++ Reader.lua
@@ -4,20 +4,18 @@ local FLOAT_PRECISION = 24
 local Reader = {}
 
 function Reader.new(bytecode)
-	local pos = 1
+	local stream = buffer.fromstring(bytecode)
+	local cursor = 0
 	--
 	local self = {}
 
 	function self:len()
-		return string.len(bytecode)
+		return buffer.len(stream)
 	end
 
-	function self:nextByte(peek, customPos)
-		local use = customPos and customPos or pos
-		local result = string.byte(bytecode, use, use)
-		if not peek then
-			pos += 1
-		end
+	function self:nextByte()
+		local result = buffer.readu8(stream, cursor)
+		cursor += 1
 		return result
 	end
 	function self:nextBytes(count)
@@ -34,54 +32,48 @@ function Reader.new(bytecode)
 	end
 
 	function self:nextUInt32()
-		local a, b, c, d = self:nextByte(), self:nextByte(), self:nextByte(), self:nextByte()
-		local result = bit32.lshift(d, 24) + bit32.lshift(c, 16) + bit32.lshift(b, 8) + a
+		local result = buffer.readu32(stream, cursor)
+		cursor += 4
 		return result
 	end
-	function self:nextInt32(peek)
-		if peek then
-			local a, b, c, d = self:nextByte(true, pos), self:nextByte(true, pos+1), self:nextByte(true, pos+2), self:nextByte(true, pos+3)
-			local result = bit32.lshift(bit32.band(d, 0xFF), 24) + bit32.lshift(bit32.band(c, 0xFF), 16) + bit32.lshift(bit32.band(b, 0xFF), 8) + bit32.band(a, 0xFF)
-			return result
-		else
-			local a, b, c, d = self:nextByte(), self:nextByte(), self:nextByte(), self:nextByte()
-			local result = bit32.lshift(bit32.band(d, 0xFF), 24) + bit32.lshift(bit32.band(c, 0xFF), 16) + bit32.lshift(bit32.band(b, 0xFF), 8) + bit32.band(a, 0xFF)
-			return result
-		end
+	function self:nextInt32()
+		local result = buffer.readi32(stream, cursor)
+		cursor += 4
+		return result
 	end
 
 	function self:nextFloat()
-		local a, b, c, d = self:nextChar(), self:nextChar(), self:nextChar(), self:nextChar()
-		local result = tonumber(string.format(`%0.{FLOAT_PRECISION}f`, string.unpack("f", a .. b .. c .. d)))
-		return result
+		local result = buffer.readf32(stream, cursor)
+		cursor += 4
+		return tonumber(string.format(`%0.{FLOAT_PRECISION}f`, result))
 	end
 
 	function self:nextVarInt()
-		local result, shift = 0, 0
-		local b
-		repeat
-			b = self:nextByte()
-			result = bit32.bor(result, bit32.lshift(bit32.band(b, 0x7F), shift))
-			shift += 7
-		until not bit32.btest(b, 0x80)
+		local result = 0
+		for i = 0, 4 do
+			local b = self:nextByte()
+			result = bit32.bor(result, bit32.lshift(bit32.band(b, 0x7F), i * 7))
+			if not bit32.btest(b, 0x80) then
+				break
+			end
+		end
 		return result
 	end
 
 	function self:nextString(len)
 		len = len or self:nextVarInt()
-		local result = ""
-		for i = 1, len do
-			result ..= self:nextChar()
+		if len == 0 then
+			return ""
+		else
+			local result = buffer.readstring(stream, cursor, len)
+			cursor += len
+			return result
 		end
-		return result
 	end
 
 	function self:nextDouble()
-		local str = ""
-		for i = 1, 8 do
-			str ..= string.char(self:nextByte())
-		end
-		local result = string.unpack("<d", str)
+		local result = buffer.readf64(stream, cursor)
+		cursor += 8
 		return result
 	end
 
