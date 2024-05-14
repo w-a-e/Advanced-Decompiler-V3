53b9c32079292f011f5084135c80139311b4182e
wae <69380106+w-a-e@users.noreply.github.com>
2024-05-14 08:50:31 +0300

    add timeout string
    𝓭𝓮𝓬𝓸𝓶𝓹𝓲𝓵𝓮𝓻

--- Strings.lua
+++ Strings.lua
@@ -1,8 +1,9 @@
 -- Collection of strings later formatted by the decompiler for output
-local MemeStrings = {" boo 👻", " Exceeded decompiler timeout.", " DECOMPILED BY ADVANCED DECOMPILER V3", " Decompiled with the Synapse X Luau decompiler.", "SynapseX Decompiler", "decompiler is slow, removed right now :(", "NOTE: Currently in beta! Not representative of final product.", " params : ...", " " .. os.date(), " your advertisement could be here"}
+local MemeStrings = {" boo 👻", " Exceeded decompiler timeout.", " DECOMPILED BY ADVANCED DECOMPILER V3", " Decompiled with the Synapse X Luau decompiler.", " 𝓓𝓮𝓬𝓸𝓶𝓹𝓲𝓵𝓮𝓭 𝓫𝔂 𝓯𝓻𝓮𝓪𝓴𝔂 𝓭𝓮𝓬𝓸𝓶𝓹𝓲𝓵𝓮𝓻", "SynapseX Decompiler", "decompiler is slow, removed right now :(", "NOTE: Currently in beta! Not representative of final product.", " params : ...", " " .. os.date(), " your advertisement could be here"}
 
 local Strings = {
 	SUCCESS = "--" .. MemeStrings[math.random(#MemeStrings)] .. "\n%s",
+	TIMEOUT = "-- DECOMPILER TIMEOUT",
 	COMPILATION_FAILURE = "-- SCRIPT FAILED TO COMPILE, ERROR:\n%s",
 	UNSUPPORTED_LBC_VERSION = "-- PASSED BYTECODE IS TOO OLD AND IS NOT SUPPORTED",
 	USED_GLOBALS = "-- USED GLOBALS: %s.\n",
