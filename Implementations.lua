f59e7155f78ed0b8241c17509a1ccdd1033bdd69
wae <69380106+w-a-e@users.noreply.github.com>
2025-01-01 01:28:31 +0300

    add padLeft, padRight

--- Implementations.lua
+++ Implementations.lua
@@ -3,7 +3,7 @@ local _ENV = (getgenv or getfenv)()
 local Implementations = {}
 
 -- from number to boolean
-function Implementations.toboolean(n)
+function Implementations.toBoolean(n)
 	return n ~= 0
 end
 
@@ -12,16 +12,17 @@ function Implementations.toEscapedString(s)
 	if type(s) == "string" then
 		local hasQuotationMarks = string.find(s, '"') ~= nil
 		local hasApostrophes = string.find(s, "'") ~= nil
+
 		if hasQuotationMarks and hasApostrophes then
-			return `[[{s}]]`
+			return "[[" ..s.. "]]"
 		elseif hasQuotationMarks and not hasApostrophes then
-			return `'{s}'`
-		else
-			return `"{s}"`
+			return "'" ..s.. "'"
 		end
-	else
-		return tostring(s)
+
+		return '"' ..s.. '"'
 	end
+
+	return tostring(s)
 end
 
 -- picks indexing method based on characters in a string
@@ -32,9 +33,19 @@ function Implementations.formatIndexString(s)
 			return `.{s}`
 		end
 		return `["{s}"]`
-	else
-		return tostring(s)
 	end
+
+	return tostring(s)
+end
+
+-- add left side character padding to x
+function Implementations.padLeft(x, char, padding)
+	return string.rep(char, padding - #tostring(x)) .. x
+end
+
+-- add right side character padding to x
+function Implementations.padRight(x, char, padding)
+	return x .. string.rep(char, padding - #tostring(x))
 end
 
 -- returns true if passed string is a key pointing to a Roblox global
